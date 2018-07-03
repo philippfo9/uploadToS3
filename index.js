a1baacfd406169e4495fc31b531f942be13281e8
Philipp Fock <philipp.fock18@gmail.com>
2018-07-03 09:33:36 +0200

    generic

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-var AWS = require('aws-sdk'),
-    fs = require('fs');
+let AWS = require('aws-sdk'),
+    fs = require('fs'),
+    file = "",
+    bucket = "";
 
-var file = "win32/latest.yml";
 uploadFile(file);
 
 function uploadFile(file) {
@@ -17,21 +18,14 @@ function uploadFile(file) {
         });
         var params = {
             Body: base64data,
-            Bucket: "orderstar-release",
+            Bucket: bucket,
             Key: "benutzer-"+file,
             ServerSideEncryption: "AES256",
             ACL: 'public-read'
         };
         s3.putObject(params, function(err, data) {
-            if (err) console.log(err, err.stack); // an error occurred
-            else     console.log(data);           // successful response
-            /*
-            data = {
-             ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
-             ServerSideEncryption: "AES256",
-             VersionId: "Ri.vC6qVlA4dEnjgRV4ZHsHoFIjqEMNt"
-            }
-            */
+            if (err) console.log("err", err, err.stack); // an error occurred
+            else     console.log("success", data);           // successful response
         }).on('httpUploadProgress', function (progress) {
                       console.log(progress);       });
 
